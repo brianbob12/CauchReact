c53a45c0bc0d2c0a8cd0968fa865d96da8dfc2f9
Cyrus Signer <japaneserhino@gmail.com>
2021-02-18 15:35:19 +0000

    Move WeekListView header to separate file for later use.

---
+++ src/Components/Headers/MultipurposeAddHeader.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { View, Text, TouchableWithoutFeedback } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
+
+export default ({ text, onPlusButton }) => {
+  return (
+    <View>
+      <View
+        style={{
+          width: "100%",
+          height: 50,
+          backgroundColor: "#FFFFFFFF",
+          borderBottomColor: "#d8d8d8",
+          borderBottomWidth: 1,
+          justifyContent: "space-between",
+          alignContent: "center",
+          flexDirection: "row"
+        }}
+      >
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <Ionicons name="reorder-four" size={35} color="gray" />
+        </View>
+        <View style={{ flex: 4, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
+          <Text style={{ fontSize: 15 }}>{text}</Text>
+        </View>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }} >
+          <TouchableWithoutFeedback onPress={() => {
+            setAddTaskPopup(true)
+            setSelectedTask(null)
+          }}>
+            <Ionicons name="add" size={35} color="gray" />
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+    </View>
+  )
+}
