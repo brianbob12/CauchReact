2609097c9e5c192486a8d3a423d89a33923f7f3c
Cyrus Signer <japaneserhino@gmail.com>
2021-03-05 07:57:33 +0000

    Adding react-native-Paper

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from "react-native-paper"
 
 //import screens
 
@@ -24,87 +25,87 @@ const Tab = createBottomTabNavigator();
 export default function App() {
 
   return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === 'Home') {
+                return (
+                  <Ionicons
+                    name={
+                      focused
+                        ? 'checkbox'
+                        : 'checkbox-outline'
+                    }
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === 'Classroom') {
+                return (
+                  <Ionicons
+                    name={
+                      focused
+                        ? 'log-in'
+                        : 'log-in-outline'
+                    }
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Pomodoro") {
+                return (
+                  <Ionicons
+                    name={
+                      focused
+                        ? 'timer'
+                        : 'timer-outline'
+                    }
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === 'Repeating Tasks') {
+                return (
+                  <Ionicons
+                    name={
+                      focused
+                        ? 'repeat'
+                        : 'repeat-outline'}
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === 'Analytics') {
+                return (
+                  <Ionicons
+                    name={
+                      focused
+                        ? 'analytics'
+                        : 'analytics-outline'}
+                    size={size}
+                    color={color}
+                  />
+                );
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "#00a9d4FF",
+            inactiveTintColor: "gray",
 
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            if (route.name === 'Home') {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? 'checkbox'
-                      : 'checkbox-outline'
-                  }
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === 'Classroom') {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? 'log-in'
-                      : 'log-in-outline'
-                  }
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === "Pomodoro") {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? 'timer'
-                      : 'timer-outline'
-                  }
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === 'Repeating Tasks') {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? 'repeat'
-                      : 'repeat-outline'}
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === 'Analytics') {
-              return (
-                <Ionicons
-                  name={
-                    focused
-                      ? 'analytics'
-                      : 'analytics-outline'}
-                  size={size}
-                  color={color}
-                />
-              );
-            }
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "#00a9d4FF",
-          inactiveTintColor: "gray",
-
-        }}
-        initialRouteName={"Home"}
-      >
-        <Tab.Screen name="Pomodoro" component={PomodoroScreen} />
-        <Tab.Screen name="Classroom" component={ClassroomScreen} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Repeating Tasks" component={RepeatingScreen} />
-        <Tab.Screen name="Analytics" component={AnalyticsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-
+          }}
+          initialRouteName={"Home"}
+        >
+          <Tab.Screen name="Pomodoro" component={PomodoroScreen} />
+          <Tab.Screen name="Classroom" component={ClassroomScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Repeating Tasks" component={RepeatingScreen} />
+          <Tab.Screen name="Analytics" component={AnalyticsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
